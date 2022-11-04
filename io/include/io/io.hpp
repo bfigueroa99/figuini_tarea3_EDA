b91b119630020f969d21c2c1a36fcfd7218af372
bfigueroa99 <bfigueroa@miuandes.cl>
2022-11-04 09:46:34 -0300

    delete lo no necesario

--- io/include/io/io.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef IO_IO_HPP
-#define IO_IO_HPP
-#include <iostream>
-
-namespace io {
-	void readTextFile(const std::string &filename);
-}
-
-#endif
--- io/tests/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "io/io.hpp"
-
-int main(int nargs, char** vargs){
-	std::string filename("../data/ej1.html");
-	io::readTextFile(filename);
-	return 0;
-}
